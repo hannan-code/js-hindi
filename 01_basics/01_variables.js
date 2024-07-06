ad47f672f97bad1c9a4862dabb47789c78d782f0
hannan-code <hannanpukhta@gmail.com>
2024-07-06 06:22:48 +0000

    Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "hannan@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2 //
+accountId = "35456546"
+accountEmail = "hc@amazon.com"
+accountPassword = "65784356874"
+accountCity = "blr"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treats all JS code as newer version
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolian => true/false
+//null => standalone value (type is object)
+// undefined =>  (type is undefined)
+// symbol => unique
+
+
+//object 
+
+console.log(typeof age);
